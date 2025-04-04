6644407d3c1ed2729e6a5b9ead159dcaf77e6230
Jaero <jaeroboctaviano3@gmail.com>
2025-04-04 21:05:15 +0800

    feat: initialize server

---
+++ main/data_engineer/dashboard/national/national_page.py
@@ -0,0 +1,6 @@
+from dash import html, dcc
+
+def nationalPage():
+    return ([
+        html.P('hi')
+    ])
--- main/data_engineer/dashboard_page/national/national_page.py
+++
@@ -1,4 +0,0 @@
-from dash import html, dcc
-
-def nationalPage():
-    return html.P('national page')
